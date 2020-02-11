fd19ab53fcd5f61c3dae1ae2b2cca8a4ffe7da2b
jace-kline <jace_kline@ku.edu>
2020-02-10 18:07:40 -0600

    Probs 1 and 2

--- ShiftCipher.hs
+++ ShiftCipher.hs
@@ -1,5 +1,7 @@
 module Shift where
 
+import Data.List (sortBy)
+
 chars = ['a'..'z']
 char_num = zip chars [0..25]
 num_char = zip [0..25] chars
@@ -17,4 +19,37 @@ caesarDecrypt = decryptStr 3
 findShift enc plain = ((toNum enc) - (toNum plain)) `mod` 26
 tryDecrypt enc plain = decryptStr (findShift enc plain)
 
-c = "fqjcb rwjwj vnjax bnkhj whxcq nawjv nfxdu mbvnu ujbbf nnc"
+letter_count c s = foldr (\x rest -> (if x == c then 1 else 0) + rest) 0 s
+letter_counts s = [(c, letter_count c s) | c <- ['a'..'z']]
+
+frequency_order s = (fst . unzip) (sortBy f (letter_counts s))
+    where f (_,x1) (_,x2) = compare x2 x1
+
+str1 = "fqjcb rwjwj vnjax bnkhj whxcq nawjv nfxdu mbvnu ujbbf nnc"
+
+{- PROBLEM 1:
+    > frequency_order str1
+    jnbwcfuvxahqdkmregilopstyz
+    > tryDecrypt 'j' 'e' str1
+    "alexw mrere qievs wifce rcsxl ivreq iasyp hwqip pewwa iix" 
+    -- Not plausible plaintext, try next
+    > tryDecrypt 'n' 'e' str1
+    "whats inana mearo sebya nyoth ernam ewoul dsmel lassw eet"
+
+    After re-chunking the decrypted string str, we find that the plaintext message is...
+    "whats in a name arose by any other name would smell just as sweet"
+-}
+
+str2 = "oczmz vmzor jocdi bnojv dhvod igdaz admno ojbzo rcvot jprvi oviyv aozmo cvooj ziejt dojig toczr dnzno jahvi fdiyv xcdzq zoczn zxjiy"
+
+{- PROBLEM 2:
+    > frequency_order str2
+    "ozvdijcnamrtybghxefpqklsuw"
+    > tryDecrypt 'o' 'e' str2
+    "espcp lcpeh zesty rdezl txlet ywtqp qtcde ezrpe hslej zfhly elyol qepce sleez pyuzj tezyw jesph tdpde zqxly vtyol nstpg pespd pnzyo"
+    -- Not plausible plaintext, try next
+    > tryDecrypt 'z' 'e' str2
+    "there aretw othin gstoa imati nlife first toget whaty ouwan tanda ftert hatto enjoy itonl ythew isest ofman kinda chiev ethes econd"
+
+
+-}
